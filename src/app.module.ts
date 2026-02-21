dbfa13178897d1bdad86773407eaaeb7de70cbf8
IhsanJati <ihsanjati127@gmail.com>
2026-02-21 13:51:01 +0700

    feat: add user and auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [CommonModule],
+  imports: [CommonModule, UsersModule, AuthModule],
   controllers: [],
   providers: [],
 })
