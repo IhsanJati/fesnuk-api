60253d17b49507fbfa858bb2b0917876f0f2aa38
IhsanJati <ihsanjati127@gmail.com>
2026-02-24 21:11:17 +0700

    refactor: improve CurrentUser decorator to enhance readability and maintainability

--- src/common/current-user.decorator.ts
+++ src/common/current-user.decorator.ts
@@ -4,6 +4,8 @@ import { JwtPayload, RequestWithUser } from 'src/model/auth.model';
 export const CurrentUser = createParamDecorator(
   (data: keyof JwtPayload | undefined, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest<RequestWithUser>();
-    return request.user;
+    const user = request.user;
+
+    return data ? user?.[data] : user;
   },
 );
