02a868dbe1417bf7951ef82de7ebae41c507d34e
IhsanJati <ihsanjati127@gmail.com>
2026-02-28 13:39:29 +0700

    fix: remove metadata type validation

--- src/common/zod.pipe.ts
+++ src/common/zod.pipe.ts
@@ -9,11 +9,8 @@ import { ZodType } from 'zod';
 @Injectable()
 export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: ZodType) {}
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   transform<T>(value: T, metadata: ArgumentMetadata) {
-    if (metadata.type !== 'body') {
-      return value;
-    }
-
     const parseResult = this.schema.safeParse(value);
 
     if (!parseResult.success) {
