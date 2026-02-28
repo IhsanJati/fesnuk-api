ffd373c0b05e828f5f3c9bc1c10700ebb3e1c5da
IhsanJati <ihsanjati127@gmail.com>
2026-02-28 13:28:08 +0700

    feat: add meta object response for pagination

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -2,6 +2,11 @@ export interface UserResponse {
   success: boolean;
   message?: string;
   data?: object;
+  meta?: {
+    page: number;
+    limit: number;
+    totalPages: number;
+  };
   error?: {
     code: string;
     messsage: string;
