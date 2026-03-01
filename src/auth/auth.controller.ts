1357479b5dda8c5029516962921e035be190e3ff
IhsanJati <ihsanjati127@gmail.com>
2026-03-01 11:22:28 +0700

    refactor: standardize input validation with zod schemas

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,24 +1,19 @@
 import {
   Body,
   Controller,
-  Get,
   HttpCode,
   HttpStatus,
   Post,
-  UseGuards,
   UsePipes,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { signInSchema, type SignInDto } from './dto/signIn.schema';
-import { AuthGuard } from './auth.guard';
+import { signInSchema, type SignInDto } from './schemas/signIn.schema';
 import { UsersService } from 'src/users/users.service';
-import { CurrentUser } from 'src/common/current-user.decorator';
-import type { JwtPayload } from 'src/model/auth.model';
 import { ZodValidationPipe } from 'src/common/zod.pipe';
 import {
   type CreateUserRequest,
   registerUserSchema,
-} from 'src/users/dto/create-user.schema';
+} from 'src/users/schemas/create-user.schema';
 import { UserResponse } from 'src/model/user.model';
 
 @Controller('/api/auth')
@@ -39,11 +34,5 @@ export class AuthController {
   @UsePipes(new ZodValidationPipe(signInSchema))
   signIn(@Body() SignInDto: SignInDto): Promise<UserResponse> {
     return this.authService.signIn(SignInDto.email, SignInDto.password);
-  }
-
-  @Get('/profile')
-  @UseGuards(AuthGuard)
-  async getProfile(@CurrentUser() user: JwtPayload) {
-    return await this.userService.getUserById(user.sub);
   }
 }
